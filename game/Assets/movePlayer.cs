414db6452ef44e3662059b60da253d9f5461aa22
Xavier <njsandilands@gmail.com>
2024-04-04 15:08:14 -0500

    cat animation and candle colliders

--- game/Assets/movePlayer.cs
+++ game/Assets/movePlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
- 
+
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
@@ -10,36 +10,69 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer Sr;
 
     Vector2 movement;
-    [SerializeField] private float RunSpeed = 7f;
- 
-    private enum MovementState { idle, running, jumping, falling }
- 
+    [SerializeField] private float WalkSpeed = 7f;
+    [SerializeField] private float RunSpeed = 14f;
+
+    private enum MovementState { idle, walking }
+    private MovementState state = MovementState.idle;   
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         Sr = GetComponent<SpriteRenderer>();
     }
- 
+
     // Update is called once per frame
     void Update()
     {
- 
+
         PlayerControlUpdate();
         FixedPlayerUpdate();
+        AnimationUpdate(GetState());
     }
- 
+
     private void PlayerControlUpdate()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
+
     }
 
     private void FixedPlayerUpdate()
     {
-        rb.MovePosition(rb.position + movement * RunSpeed * Time.fixedDeltaTime);
+        
+            rb.MovePosition(rb.position + movement * WalkSpeed * Time.fixedDeltaTime);
+       
+    }
+
+    private MovementState GetState()
+    {
+        return state;
+    }
+
+    private void AnimationUpdate(MovementState state)
+    {
+      
+
+        if (movement.x > 0 || movement.y > 0)
+        {
+            Sr.flipX = false;
+           
+                state = MovementState.walking;
+            
+        }
+        if (movement.x < 0 || movement.y < 0)
+        {
+            Sr.flipX = true;
+           
+           
+                state = MovementState.walking;
+            
+        }
+
+        anim.SetInteger("state", value: (int)state);
     }
 }
