fa452b0fa7d6d96a5af5e0f5d9503b3e4652e46c
Xavier <njsandilands@gmail.com>
2024-04-24 09:50:00 -0500

    carry over

---
+++ game/Assets/GameState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameState", menuName = "Scri[tableObjects/GameSate", order = 0)]
+public class GameState : ScriptableObject
+{
+    
+    
+}
