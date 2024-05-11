125c3de657dbf992be95a68b722530d06f56c74f
Xavier <njsandilands@gmail.com>
2024-05-11 14:06:10 -0500

    input scene change

--- game/Assets/scripts/Interact trigger point.cs
+++ game/Assets/scripts/Interact trigger point.cs
@@ -1,39 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Interacttriggerpoint : MonoBehaviour
 {
-    [SerializeField] public bool goNextScene;
-    [SerializeField] public string levelName;
-    [SerializeField] public bool needInput;
-    private bool isInCollider;
-    //[SerializeField] public Collider2D collision;
-
-
-    private bool isNextScene = true;
-    [SerializeField] public SceneInfo sceneInfo;
-
-    private PlayerMovement playerInput;
-    //private bool interact = PlayerMovement.IsInteracted();
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private string next;
+    private void OnTriggerStay2D(Collider2D collision)
     {
-
-        if (collision.CompareTag("Player"))
+        if(collision.CompareTag("Player"))
         {
-
-            if (playerInput.IsInteracted() == true)
+            if(Input.GetKeyDown(KeyCode.E)) 
             {
-                if (!isNextScene)
-                {
-                    SceneController.instance.NextScene();
-                }
-                else
-                {
-                    SceneController.instance.LoadScene(levelName);
-                }
+                SceneManager.LoadScene(next);
             }
         }
-
     }
 }
