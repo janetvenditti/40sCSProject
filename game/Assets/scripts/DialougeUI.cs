8bf2ac003ddcb156d4a62ddc4fc47f6f63716b21
JanetVABC <janetvenditti6@gmail.com>
2024-05-22 10:43:23 -0500

    Fixing console errors

--- game/Assets/scripts/DialougeUI.cs
+++ game/Assets/scripts/DialougeUI.cs
@@ -1,14 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro; //used to control the keyboard effect and most of the actual text 
+
 public class DialougeUI : MonoBehaviour
 {
-    [SerializeField] private TMP_Text textLabel;
-
-    //method to draw text in 
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-       GetComponent<TypeWritterEffect>().Run("This is text\n Hey", textlabel);
-       // textLabel.text = "Hello\nThis is the next line.";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
