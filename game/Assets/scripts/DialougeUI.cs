68d1c620a09506de7fafae3a1b0effcda241fc9e
Xavier <njsandilands@gmail.com>
2024-05-23 14:47:52 -0500

    commit

--- game/Assets/scripts/DialougeUI.cs
+++ game/Assets/scripts/DialougeUI.cs
@@ -3,12 +3,11 @@ using TMPro; //used to control the keyboard effect and most of the actual text
 public class DialougeUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text textLabel;
-
     //method to draw text in 
 
     private void Start()
     {
-       GetComponent<TypeWritterEffect>().Run("This is text\n Hey", textlabel);
+       GetComponent<TypeWritterEffect>().Run("This is text\n Hey", textLabel);
        // textLabel.text = "Hello\nThis is the next line.";
     }
 }
