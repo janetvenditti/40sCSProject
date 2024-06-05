3f997252cc6f73a1b0d02a893510055d8f4fba04
JanetVABC <janetvenditti6@gmail.com>
2024-06-04 23:34:07 -0500

    Total changes

--- game/Assets/scripts/TypewriterEffect.cs
+++ game/Assets/scripts/TypewriterEffect.cs
@@ -1,46 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections; //lets us go through a list of collections, such as our writing objects!
 using UnityEngine;
 using TMPro;
 
-public class TypewriterEffect : MonoBehaviour
+public class TypewriterEffect : MonoBehaviour //make a new class that inherits from monobehaviour so it can be used with game objects / components like the "test"
 {
-     [SerializeField] private float typewriterSpeed = 50f;
-    //starts the routine (runs it)
-    // string to type, then label to write it on
-   public Coroutine Run(string textToType, TMP_Text textLabel)
-   {
-        return StartCoroutine(routine: TypeText(textToType, textLabel));
+    [SerializeField] private float typewriterSpeed = 50f; //make a private variable that can be edited on unity 
 
-   }
+    public Coroutine Run(string textToType, TMP_Text textLabel) //a coroutine method can pause to return new text lines 
+    {
+        return StartCoroutine(TypeText(textToType, textLabel));
+    }
 
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+        textLabel.text = string.Empty;
+        int charIndex = 0;    //track index of charecter being typed along 
 
-    //types the text onto the label
-     private IEnumerator TypeText(string textToType, TMP_Text textLabel)
-     {
-          textLabel.text = string.Empty;
-          float t = 0;
-          // Used to understand how many chars are typed at once
-          int charIndex = 0;
+        float delay = 1 / typewriterSpeed;   // delay with typing a character based on typewriterSpeed
 
-          while (charIndex < textToType.Length)
-          {
-               t += Time.deltaTime * typewriterSpeed;
-               // Increment charIndex based on time
-               charIndex = Mathf.FloorToInt(Time.time);
+        while (charIndex < textToType.Length)
+        {
+     
+            textLabel.text += textToType[charIndex];    //do next character
+            charIndex++;
 
-               // Only typing for the message duration needed 
-               charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
-
-               // Text left to type 
-               textLabel.text = textToType.Substring(0, charIndex);
-
-               // Wait one frame
-               yield return null; 
-          }
-
-          // Set the label to the complete text after typing finishes
-          textLabel.text = textToType;
-     }
-
+            yield return new WaitForSeconds(delay);     // wait for the time of delay
+        }
+    }
 }
